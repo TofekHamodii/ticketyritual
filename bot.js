8f05e3ed97a955e365810107d799c3ef1bcd322c
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-10-29 19:55:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,4 +130,12 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
 });
 
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('506526379748098049');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online : ${currentSize} `);
+  if (currentSize !== size) channel.setName(`Voice Online : ${currentSize} `);
+});
+
 client.login(process.env.BOT_TOKEN);
